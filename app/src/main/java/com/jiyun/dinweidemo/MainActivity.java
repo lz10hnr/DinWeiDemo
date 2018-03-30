113a48f7bd81283bc6b3a15f140cf0a0c358bb66
lz10hnr <923622583@.com>
2018-03-30 09:50:21 +0800

    Initial commit

---
+++ app/src/main/java/com/jiyun/dinweidemo/MainActivity.java
@@ -0,0 +1,327 @@
+package com.jiyun.dinweidemo;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.geocoder.AoiItem;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeAddress;
+import com.amap.api.services.geocoder.RegeocodeQuery;
+import com.amap.api.services.geocoder.RegeocodeResult;
+import com.amap.api.services.geocoder.StreetNumber;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LocationSource, AMapLocationListener, View.OnClickListener, AMap.OnCameraChangeListener, GeocodeSearch.OnGeocodeSearchListener {
+
+    private TextView mWeiZhi_Yes;
+    private MapView map;
+    private Toolbar mToolbar;
+    private AMap aMap;
+    private MyLocationStyle myLocationStyle;
+    private OnLocationChangedListener mListener;
+    private AMapLocationClientOption mLocationOption;
+    private EditText mEditText;
+    private Button mQingK;
+    private MarkerOptions markerOption = new MarkerOptions();
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mlocationClient = null;
+    GeocodeSearch geocoderSearch;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+
+        @Override
+        public void onLocationChanged(AMapLocation amapLocation) {
+
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                    //获取纬度
+                    latitude = amapLocation.getLatitude();
+                    //获取经度
+                    longitude = amapLocation.getLongitude();
+                    String city = amapLocation.getCity();
+                    aoiName = amapLocation.getAoiName();
+                    String district = amapLocation.getDistrict();
+                    String streetNum = amapLocation.getStreetNum();
+//                    mEditText.setText(aoiName +"("+ city +district+")"+streetNum);
+                    initMarkerOption();
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError","location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo());
+                }
+            }
+        }
+    };
+    private double latitude;
+    private double longitude;
+    private Marker marker;
+    boolean is = true;
+    private String aoiName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        map = (MapView) findViewById(R.id.map);
+        map.onCreate(savedInstanceState);
+        initView();
+        showMap();
+        initLocation();
+        geocoderSearch = new GeocodeSearch(MainActivity.this);
+        geocoderSearch.setOnGeocodeSearchListener(this);
+    }
+
+    private void initMarkerOption() {
+        initMapInc();
+//        if (marker != null){
+//            marker.remove();
+//        }
+        if (is){
+            marker = aMap.addMarker(markerOption);
+            is = !is;
+        }
+        AMap.OnMarkerDragListener markerDragListener = new AMap.OnMarkerDragListener() {
+
+            // 当marker开始被拖动时回调此方法, 这个marker的位置可以通过getPosition()方法返回。
+            // 这个位置可能与拖动的之前的marker位置不一样。
+            // marker 被拖动的marker对象。
+            @Override
+            public void onMarkerDragStart(Marker arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            // 在marker拖动完成后回调此方法, 这个marker的位置可以通过getPosition()方法返回。
+            // 这个位置可能与拖动的之前的marker位置不一样。
+            // marker 被拖动的marker对象。
+            @Override
+            public void onMarkerDragEnd(Marker arg0) {
+                // TODO Auto-generated method stub
+                LatLng position = arg0.getPosition();
+
+                aMap.moveCamera(CameraUpdateFactory.changeLatLng(position));
+            }
+
+            // 在marker拖动过程中回调此方法, 这个marker的位置可以通过getPosition()方法返回。
+            // 这个位置可能与拖动的之前的marker位置不一样。
+            // marker 被拖动的marker对象。
+            @Override
+            public void onMarkerDrag(Marker arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+// 绑定marker拖拽事件
+        aMap.setOnMarkerDragListener(markerDragListener);
+
+    }
+
+    private void initMapInc() {
+        markerOption.position(new LatLng(latitude,longitude));
+        markerOption.draggable(true);//设置Marker可拖动
+        markerOption.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory
+                .decodeResource(getResources(),R.mipmap.hongd)));
+        // 将Marker设置为贴地显示，可以双指下拉地图查看效果
+        markerOption.setFlat(true);//设置marker平贴地图效果\
+    }
+
+    private void initLocation() {
+
+
+    }
+
+    private void showMap() {
+
+        if (aMap == null) {
+            aMap = map.getMap();
+        }
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.mipmap.land));
+        myLocationStyle.strokeWidth(0);
+        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));// 设置圆形的填充颜色
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setZoomControlsEnabled(false);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
+        aMap.getUiSettings().setRotateGesturesEnabled(false);
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(12));
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        // 设置定位监听
+        aMap.setLocationSource(this);
+// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+// 设置定位的类型为定位模式，有定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+        aMap.setOnCameraChangeListener(this);
+    }
+
+    private void initView() {
+        mWeiZhi_Yes = (TextView) findViewById(R.id.mWeiZhi_Yes);
+        mToolbar = (Toolbar) findViewById(R.id.mToolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        mEditText = (EditText) findViewById(R.id.mEditText);
+        mEditText.setOnClickListener(this);
+        mEditText.setInputType(InputType.TYPE_NULL);
+        mQingK = (Button) findViewById(R.id.mQingK);
+        mQingK.setOnClickListener(this);
+    }
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            //初始化定位
+            mlocationClient = new AMapLocationClient(this);
+            //初始化定位参数
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位回调监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mLocationOption.setHttpTimeOut(20000);
+            mLocationOption.setNeedAddress(true);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            mlocationClient.setLocationListener(mLocationListener);
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            mlocationClient.startLocation();//启动定位
+        }
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null
+                    && amapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr", errText);
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        map.onDestroy();
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mQingK:
+                mEditText.setText("");
+                break;
+            case R.id.mWeiZhi_Yes:
+                submit();
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String mEditTextString = mEditText.getText().toString().trim();
+        if (TextUtils.isEmpty(mEditTextString)) {
+            Toast.makeText(this, "请输入正确地址", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
+    }
+
+    @Override
+    public void onCameraChange(CameraPosition cameraPosition) {
+        LatLng target = cameraPosition.target;
+        double latitude = target.latitude;
+        marker.remove();
+        initMapInc();
+        markerOption.position(target);
+
+        marker = aMap.addMarker(markerOption);
+    }
+
+    @Override
+    public void onCameraChangeFinish(CameraPosition cameraPosition) {
+        LatLng target = cameraPosition.target;
+        // 第一个参数表示一个Latlng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
+        RegeocodeQuery query = new RegeocodeQuery(new LatLonPoint(target.latitude,target.longitude), 200,GeocodeSearch.AMAP);
+        geocoderSearch.getFromLocationAsyn(query);
+    }
+
+    @Override
+    public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+        RegeocodeAddress regeocodeAddress = regeocodeResult.getRegeocodeAddress();
+        String adCode = regeocodeAddress.getAdCode();
+        List<AoiItem> aois = regeocodeAddress.getAois();
+        String city = regeocodeAddress.getCity();
+        String district = regeocodeAddress.getDistrict();
+        StreetNumber streetNumber = regeocodeAddress.getStreetNumber();
+        String number = streetNumber.getNumber();
+        if (aois.size() > 0){
+            for (AoiItem aoi : aois) {
+                mEditText.setText(aoi.getAoiName()+"("+city+district+")"+"附近");
+            }
+        }
+    }
+
+    @Override
+    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+    }
+}
